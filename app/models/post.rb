408a54e4d73b4d092dadcdccba8439a8e98ee70c
Beatriz D'addead <beatrizchith@gmail.com>
2022-06-21 15:28:23 -0300

    blog

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, length: {minimum:5, maximum:20}
+    validates :description, presence: true,  length: {minimum:15, maximum:40} 
+
+    
 end
+
